93f9cd8089f8448215c6b3bb2eb1bb9ebe8344d2
Bharathgoudganagoni <bharathgoudganagoni123@gmail.com>
2026-04-12 19:32:30 +0530

    fix base path for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/NNclicks/",
+  base: "/",   // ✅ FIXED
   plugins: [react()],
   resolve: {
     alias: {
